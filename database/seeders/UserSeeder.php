eb5fc77878c4c1d243925da2d715cb7f46697122
usaikoo <info@usaiko.dev>
2023-09-30 13:06:26 -0400

    update data

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name'=>'User',
+            'email'=>'user@vshop.com',
+            'password'=>Hash::make('password'),
+           
+        ]);
+    }
+}
